6204d63e0334a2291a0bf67126a88fc9d1ffe2a3
freddychris.org <a.freddychris@gmail.com.org>
2020-12-07 10:52:04 +0530

    Day 14

---
+++ assets/14-Alternating Sums.ts
@@ -0,0 +1,17 @@
+function alternatingSums(a: number[]): number[] {
+    let evenSum = 0;
+    let oddSum = 0;
+     a.forEach((element, index) =>{
+         if(index % 2 === 0)
+         {
+             evenSum += element;
+         }else
+         {
+            oddSum +=element;
+         }
+     });
+     return [evenSum, oddSum];
+
+}
+console.log(alternatingSums([50,60,60,45,70]))
+console.log(alternatingSums([1,2,3,4,5,6,7,8,9,10]))
